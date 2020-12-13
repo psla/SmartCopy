eb9e8a9097a8fc1a2c350e3930c3dc8bb4ad8d17
Piotr (Peter) Slatala <psla-github@sepio.pl>
2020-12-13 15:07:25 -0800

    Get the next available file

---
+++ CopyToSubdir.Test/FilenameGeneratorTests.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace CopyToSubdir.Test
+{
+    public class FilenameGeneratorTests
+    {
+        [Test]
+        public void GetsTheSameNameForNonexistingFile()
+        {
+            Assert.AreEqual("res\\NonExistingFile.txt", FilenameGenerator.GetFirstAvailableName("res", "NonExistingFile.txt")); 
+        }
+
+        [Test]
+        public void GetsTheIteratedNameForExistingOne()
+        {
+            Assert.AreEqual("res\\File2-2.txt", FilenameGenerator.GetFirstAvailableName("res", "File2.txt"));
+        }
+
+        [Test]
+        public void GetsTheIteratedNameIfAnotherOneAlreadyExists()
+        {
+            Assert.AreEqual("res\\File1-3.txt", FilenameGenerator.GetFirstAvailableName("res", "File1.txt"));
+        }
+    }
+}
---
+++ CopyToSubdir/FilenameGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace CopyToSubdir
+{
+    public static class FilenameGenerator
+    {
+        /// <summary>
+        /// Given the filename "Foo.jpg", and a directory, it finds all files with the pattern "Foo.jpg" and "Foo-integer.jpg".
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<string> GetListOfCopies(string directoryPath, string basefilename)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns a first available name for the new version of the file.
+        /// </summary>
+        public static string GetFirstAvailableName(string directoryPath, string baseFilename)
+        {
+            if (!File.Exists(Path.Combine(directoryPath, baseFilename)))
+            {
+                return Path.Combine(directoryPath, baseFilename);
+            }
+
+            int indexOfDot = baseFilename.LastIndexOf(".");
+            if (indexOfDot == -1)
+            {
+                // We require extension, although technically it shouldn't be needed.
+                throw new ArgumentException("Extension is required");
+            }
+
+            string filenameNoExtension = baseFilename.Substring(0, indexOfDot);
+            string extension = baseFilename.Substring(indexOfDot + 1);
+
+            int startingIndex = 2;
+
+            while (File.Exists(Path.Combine(directoryPath, CombineFilename(filenameNoExtension, startingIndex, extension))))
+            {
+                startingIndex++;
+            }
+
+            return Path.Combine(directoryPath, CombineFilename(filenameNoExtension, startingIndex, extension));
+        }
+
+        private static string CombineFilename(string prefix, int index, string extension)
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0}-{1}.{2}", prefix, index, extension);
+        }
+    }
+}
